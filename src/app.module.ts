3448adcf37ddab679633a39b9bc0225f95bd664b
Richard Carrion <richard.carrion.r@gmail.com>
2022-09-02 00:52:06 -0500

    Res base metric

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { OrganizationModule } from './organization/organization.module';
 import { TribeModule } from './tribe/tribe.module';
 import { RepositoryModule } from './repository/repository.module';
+import { MetricModule } from './metric/metric.module';
 
 @Module({
-  imports: [OrganizationModule, TribeModule, RepositoryModule],
+  imports: [OrganizationModule, TribeModule, RepositoryModule, MetricModule],
   controllers: [AppController],
   providers: [AppService],
 })
