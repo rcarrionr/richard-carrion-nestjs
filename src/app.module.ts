1f0c05ac53ac97bff8d3c22a56816abb00893b9e
Richard Carrion <richard.carrion.r@gmail.com>
2022-09-02 00:51:38 -0500

    Res base repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationModule } from './organization/organization.module';
 import { TribeModule } from './tribe/tribe.module';
+import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [OrganizationModule, TribeModule],
+  imports: [OrganizationModule, TribeModule, RepositoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
