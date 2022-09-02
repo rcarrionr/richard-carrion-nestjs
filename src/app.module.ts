16002e9461be4577403a8bf550455935a50bb290
Richard Carrion <richard.carrion.r@gmail.com>
2022-09-02 01:08:14 -0500

    Configuracion Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrganizationModule } from './organization/organization.module';
 import { TribeModule } from './tribe/tribe.module';
 import { RepositoryModule } from './repository/repository.module';
@@ -9,6 +8,5 @@ import { MetricModule } from './metric/metric.module';
 @Module({
   imports: [OrganizationModule, TribeModule, RepositoryModule, MetricModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
