4921531d6bc731fc6ee2a62e9583dd45399d06cc
Richard Carrion <richard.carrion.r@gmail.com>
2022-09-02 22:58:28 -0500

    Servicio ORM Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { OrganizationModule } from './organization/organization.module';
-import { TribeModule } from './tribe/tribe.module';
-import { RepositoryModule } from './repository/repository.module';
-import { MetricModule } from './metric/metric.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { OrganizationModule } from "./organization/organization.module";
+import { TribeModule } from "./tribe/tribe.module";
+import { RepositoryModule } from "./repository/repository.module";
+import { MetricModule } from "./metric/metric.module";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
   imports: [OrganizationModule, TribeModule, RepositoryModule, MetricModule],
   controllers: [AppController],
+  providers: [PrismaService]
 })
-export class AppModule {}
+export class AppModule {
+}
